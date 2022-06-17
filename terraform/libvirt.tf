74331cf247742ab24c6086242036d05d76922880
J Goossens <jgoos@users.noreply.github.com>
2022-06-17 23:38:38 +0100

    Convert to use for_each instead of count

--- terraform/libvirt.tf
+++ terraform/libvirt.tf
@@ -1,55 +1,55 @@
 # Read ssh public key that is used via cloud-init
 data "local_file" "ssh" {
   filename = pathexpand("~/.ssh/id_ed25519")
-  #filename = "${path.module}/.cch/key"
 }
 
 # Defining VM Volume
 resource "libvirt_volume" "rhel9" {
-
   name   = "rhel9"
   source = "./packer-rhel-9-x86_64"
 }
 
 resource "libvirt_volume" "worker" {
-
-  name           = "worker_${count.index}.qcow2"
+  for_each       = var.vm
+  name           = "${each.key}.qcow2"
   base_volume_id = libvirt_volume.rhel9.id
-  count          = var.workers_count
 }
 
-#data "template_file" "user_data" {
-#  template = file("${path.module}/cloud_init.tmpl")
-#}
-
 data "template_file" "user_data" {
-  template = file("${path.module}/cloud_init.cfg")
+  for_each = var.vm
+  template = file("${path.module}/cloud_init.tmpl")
+  vars = {
+    hostname   = "${each.key}"
+    host_fqdn  = "${each.value.fqdn}"
+    cloud_user = "${var.cloud_user}"
+  }
 }
 
 resource "libvirt_cloudinit_disk" "commoninit" {
-  name      = "commoninit.iso"
-  user_data = data.template_file.user_data.rendered
+  for_each  = var.vm
+  name      = "${each.key}cloud-init.iso"
+  user_data = data.template_file.user_data[each.key].rendered
 }
 
 resource "libvirt_domain" "rhel9" {
-  count  = var.workers_count
-  name   = "domain_${count.index}"
-  memory = "1024"
-  vcpu   = 1
+  for_each = var.vm
+  name     = each.key
+  memory   = each.value.memory
+  vcpu     = each.value.cpu
 
-  cloudinit = libvirt_cloudinit_disk.commoninit.id
+  cloudinit = libvirt_cloudinit_disk.commoninit[each.key].id
 
   network_interface {
-    network_name = "default" # List networks with virsh net-list
+    network_name   = "default" # List networks with virsh net-list
     wait_for_lease = true
-    hostname = "domain_${count.index}"
+    hostname       = each.value.fqdn
   }
 
   cpu {
     mode = "host-model"
   }
   disk {
-    volume_id = element(libvirt_volume.worker.*.id, count.index)
+    volume_id = libvirt_volume.worker[each.key].id
   }
 
   graphics {
