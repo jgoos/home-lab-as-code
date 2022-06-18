0bcdda8e6c083795102bf2c339f427ef1ea42db0
J Goossens <jgoos@users.noreply.github.com>
2022-06-18 12:10:14 +0100

    Make ssh pubic key dynamic

--- terraform/libvirt.tf
+++ terraform/libvirt.tf
@@ -1,12 +1,8 @@
-# Read ssh public key that is used via cloud-init
-data "local_file" "ssh" {
-  filename = pathexpand("~/.ssh/id_ed25519")
-}
-
 # Defining VM Volume
 resource "libvirt_volume" "rhel9" {
   name   = "rhel9"
-  source = "./packer-rhel-9-x86_64"
+  #source = "./packer-rhel-9-x86_64"
+  source = "../rhel9/builds/packer-rhel-9-x86_64"
 }
 
 resource "libvirt_volume" "worker" {
@@ -22,6 +18,7 @@ data "template_file" "user_data" {
     hostname   = "${each.key}"
     host_fqdn  = "${each.value.fqdn}"
     cloud_user = "${var.cloud_user}"
+    ssh_pub_key_content = file("~/.ssh/${var.ssh_public_key}")
   }
 }
 
