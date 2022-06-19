d59d0676ec414929305ebe3f5db623f31baaaa7f
J Goossens <jgoos@users.noreply.github.com>
2022-06-19 15:55:44 +0100

    Added meta-data template and made storage configuration dynamic based on vars

--- terraform/libvirt.tf
+++ terraform/libvirt.tf
@@ -1,24 +1,31 @@
-# Defining VM Volume
 resource "libvirt_volume" "rhel9" {
   name   = "rhel9"
-  #source = "./packer-rhel-9-x86_64"
   source = "../rhel9/builds/packer-rhel-9-x86_64"
 }
 
 resource "libvirt_volume" "worker" {
   for_each       = var.vm
   name           = "${each.key}.qcow2"
+  size           = each.value.storage * 1024 * 1024 * 1024 # convert GB to Bytes
   base_volume_id = libvirt_volume.rhel9.id
 }
 
 data "template_file" "user_data" {
   for_each = var.vm
-  template = file("${path.module}/cloud_init.tmpl")
+  template = file("${path.module}/cloud_init_user_data.tmpl")
   vars = {
-    hostname   = "${each.key}"
-    host_fqdn  = "${each.value.fqdn}"
-    cloud_user = "${var.cloud_user}"
+    hostname            = "${each.key}"
+    host_fqdn           = "${each.value.fqdn}"
+    cloud_user          = "${var.cloud_user}"
     ssh_pub_key_content = file("~/.ssh/${var.ssh_public_key}")
+  }
+}
+
+data "template_file" "meta_data" {
+  for_each = var.vm
+  template = file("${path.module}/cloud_init_meta_data.tmpl")
+  vars = {
+    hostname            = "${each.key}"
   }
 }
 
@@ -26,18 +33,18 @@ resource "libvirt_cloudinit_disk" "commoninit" {
   for_each  = var.vm
   name      = "${each.key}cloud-init.iso"
   user_data = data.template_file.user_data[each.key].rendered
+  meta_data = data.template_file.meta_data[each.key].rendered
 }
 
 resource "libvirt_domain" "rhel9" {
-  for_each = var.vm
-  name     = each.key
-  memory   = each.value.memory
-  vcpu     = each.value.cpu
-
+  for_each  = var.vm
+  name      = each.key
+  memory    = each.value.memory
+  vcpu      = each.value.cpu
   cloudinit = libvirt_cloudinit_disk.commoninit[each.key].id
 
   network_interface {
-    network_name   = "default" # List networks with virsh net-list
+    network_name   = "default"
     wait_for_lease = true
     hostname       = each.value.fqdn
   }
@@ -52,6 +59,4 @@ resource "libvirt_domain" "rhel9" {
   graphics {
     type = "vnc"
   }
-
-  // ...
 }
